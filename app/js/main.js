97145ea8144713b58d67e47046afc3d18b0661b5
Valeriy <leshikzoloto@gmail.com>
2021-07-26 20:24:30 +0300

    fixes 12

--- app/js/main.js
+++ app/js/main.js
@@ -10,6 +10,7 @@ $(function () {
 			' <button type="button" class="slick-next"><img src="./images/slider/arrows/arrow-right.svg" alt="стрелка вправо" /></button>',
 	});
 
+	// let containerEl1 = document.querySelector('[data-ref="container-1"]');
 	let mixer = mixitup(".top-products__content", {
 		load: {
 			filter: ".category-all",
