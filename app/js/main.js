dc98c198fd023a1a3bba394473af437fc53f09ab
Valeriy <leshikzoloto@gmail.com>
2021-08-03 20:35:49 +0300

    start adaptation header

--- app/js/main.js
+++ app/js/main.js
@@ -10,7 +10,7 @@ $(function () {
 			' <button type="button" class="slick-next"><span class="sr-only">Стрелка слайдера вправо</span><img src="./images/slider/arrows/arrow-right.svg" alt="стрелка вправо" /></button>',
 	});
 
-	//----------------------------------------------
+	//----------------swiper------------------------------
 
 	const swiper = new Swiper(".partners-slider", {
 		slidesPerView: 6,
@@ -19,7 +19,8 @@ $(function () {
 		loop: true,
 	});
 
-	//----------------------------------
+	//-----------------swiper finish-----------------//
+	//mixitap----------------------------------------//
 	var containerEl1 = document.querySelector('[data-ref="container-1"]');
 	const containerEl2 = document.querySelector('[data-ref="container-2"]');
 
@@ -31,4 +32,40 @@ $(function () {
 
 	const mixer1 = mixitup(containerEl1, config);
 	const mixer2 = mixitup(containerEl2, config);
+	//-mixitap-finish--//
+	//--basket---------//
+	//ОТКРЫТИЕ КОРЗИНЫ
+	$(".header-roster__btn").on("click", function () {
+		$(".basket").addClass("basket--active");
+	});
+
+	//ОТМЕНА СТАНДАРТНОГО ПОВЕДЕНИЯ ССЫЛОК
+	$(".basket__link").on("click", function (event) {
+		event.preventDefault();
+	});
+
+	// ЗАКРЫТИЕ КОРЗИНЫ
+	$(".basket__close").on("click", function () {
+		$(".basket").removeClass("basket--active");
+	});
+	//--basket-Finish--------//
+	//ВЫПАДАЮЩИЙ СПИСОК
+	var select = $(".select"),
+		timeoutId;
+	$(".header__list").hover(
+		function () {
+			clearTimeout(timeoutId);
+			select.show(500);
+		},
+		function () {
+			timeoutId = setTimeout($.proxy(select, "hide"), 1000);
+		}
+	);
+	select
+		.mouseenter(function () {
+			clearTimeout(timeoutId);
+		})
+		.mouseleave(function () {
+			select.hide(500);
+		});
 });
